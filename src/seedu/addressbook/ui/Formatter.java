f8b68324bb82183295b6557179044d31e00c274d
unusep <unusep@gmail.com>
2016-08-31 01:51:16 +0800

    Abstract getIndexListForViewing and getIndexedListItem

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -11,6 +11,12 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
  */
 public class Formatter {
 
+    /** Format of indexed list item */
+    private static final String MESSAGE_INDEXED_LIST_ITEM = "\t%1$d. %2$s";
+
+    /** Offset required to convert between 1-indexing and 0-indexing.  */
+    public static final int DISPLAYED_INDEX_OFFSET = 1;
+    
 	/**
 	 * formatPersons takes in a list of persons and a list to store the formattedPersons,
 	 * and stores all a a list of all formatted persons in formattedPersons
@@ -22,4 +28,25 @@ public class Formatter {
             formattedPersons.add(person.getAsTextHidePrivate());
         }
 	}
+	
+    /** Formats a list of strings as a viewable indexed list. */
+    public static String getIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    private static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+
 }

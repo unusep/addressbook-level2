72660811a785764feb305af65bee5e0e8989f757
unusep <unusep@gmail.com>
2016-09-06 16:24:52 +0800

    Add UtilsTest JUnit class

---
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -0,0 +1,19 @@
+package seedu.addressbook.common;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UtilsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

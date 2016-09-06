4432e09657e29d4ad1b9ac7a13a81cc6a4da1660
unusep <unusep@gmail.com>
2016-09-06 22:00:56 +0800

    Modify name regex validator to allow commas

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -13,7 +13,7 @@ public class Name {
 
     public static final String EXAMPLE = "John Doe";
     public static final String MESSAGE_NAME_CONSTRAINTS = "Person names should be spaces or alphabetic characters";
-    public static final String NAME_VALIDATION_REGEX = "[\\p{Alpha} ]+";
+    public static final String NAME_VALIDATION_REGEX = "[\\p{Alpha} ,]+";
 
     public final String fullName;
 

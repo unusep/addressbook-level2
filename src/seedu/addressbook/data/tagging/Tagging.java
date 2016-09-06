08e1d0d09a5550a31b96fedfdacfec6b114356ef
unusep <unusep@gmail.com>
2016-09-06 13:05:54 +0800

    Add Tagging attributes and methods

--- src/seedu/addressbook/data/tagging/Tagging.java
+++ src/seedu/addressbook/data/tagging/Tagging.java
@@ -1,5 +1,22 @@
 package seedu.addressbook.data.tagging;
 
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.tag.Tag;
+
 public class Tagging {
+	Tag tag;
+	Person person;
+	boolean added;
+	
+	public Tagging(Tag t, Person p, boolean a){
+		tag = t;
+		person = p;
+		added = a;
+	}
+	
+	public String toString(){
+		return (added ? "+" : "-") + " " + 
+				person.toString() + " [" + tag.toString() + "]";
+	}
 
 }

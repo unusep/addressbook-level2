01188de464b2a84af640f6fb071666994106789c
unusep <unusep@gmail.com>
2016-09-06 13:17:07 +0800

    Fix equals method not in Contact not comparing objects properly

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -24,8 +24,8 @@ public class Contact {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Email // instanceof handles nulls
-                && this.value.equals(((Email) other).value)); // state check
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
     }
 
     @Override

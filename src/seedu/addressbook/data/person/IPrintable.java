4b942ac182f7d9630ea0584dd3cfc9c8b40fe3cf
unusep <unusep@gmail.com>
2016-09-06 12:27:16 +0800

    Add getPrintableString public method in interface

--- src/seedu/addressbook/data/person/IPrintable.java
+++ src/seedu/addressbook/data/person/IPrintable.java
@@ -1,5 +1,8 @@
 package seedu.addressbook.data.person;
 
 public interface IPrintable {
-
+	/**
+	 * @return string representing a the printable string of each object
+	 */
+	public String getPrintableString();
 }

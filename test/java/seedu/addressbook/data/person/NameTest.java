5cb377919b971b5fa7509775386e13b6c72faa14
unusep <unusep@gmail.com>
2016-09-06 21:06:32 +0800

    Add setup for NameTest test cases

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -7,9 +7,19 @@ import org.junit.Test;
 
 public class NameTest {
 
+	private Name john;
+	private Name johnWithCapitalisation;
+	private Name johnWithoutK;
+	private Name johnPermutation;
+	private Name mary;
 	
 	@Before
 	public void setUp() throws Exception {
+		john = new Name("John K Smith");
+		johnWithCapitalisation = new Name("John K SMITh");
+		johnWithoutK = new Name("John Smith");
+		johnPermutation = new Name("Smith, John K");
+		mary = new Name("Mary");
 	}
 
 	@Test
